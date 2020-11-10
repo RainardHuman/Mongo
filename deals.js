7d17c57c41a5d06f0ee755d904da91ae9e135b49
Rainard Human <rainard.human@gmail.com>
2020-11-11 00:03:11 +0200

    Update deals.js

--- deals.js
+++ deals.js
@@ -1,32 +1,53 @@
-db.deals.find({}).forEach(deal => {
+let updatedDeals = {
+    rfq: [],
+    rfp: [],
+    rfi: [],
+    po: [],
+    information: [],
+    quote: [],
+    proposal: [],
+    invoice: []
+};
+
+// todo: ========================RETRIEVE========================
+db.deals.find({ dealType: 'dealRfq' }).forEach(deal => {
     let type = '';
     let obj = {};
-    switch(deal.dealType) {
+    switch (deal.dealType) {
         case 'dealRfq': obj = _rfq(deal); type = 'rfq'; break;
-        case 'dealRfp': obj = _rfp(deal); type = 'rfp'; break;
-        case 'dealRfi': obj = _rfi(deal); type = 'rfi'; break;
-        case 'dealPo': obj = _po(deal); type = 'po'; break;
-        case 'dealInformation': obj = _information(deal); type = 'information'; break;
-        case 'dealQuote': obj = _quote(deal); type = 'quote'; break;
-        case 'dealProposal': obj = _proposal(deal); type = 'proposal'; break;
-        case 'dealInvoice': obj = _invoice(deal); type = 'invoice'; break;
+        // case 'dealRfp': obj = _rfp(deal); type = 'rfp'; break;
+        // case 'dealRfi': obj = _rfi(deal); type = 'rfi'; break;
+        // case 'dealPo': obj = _po(deal); type = 'po'; break;
+        // case 'dealInformation': obj = _information(deal); type = 'information'; break;
+        // case 'dealQuote': obj = _quote(deal); type = 'quote'; break;
+        // case 'dealProposal': obj = _proposal(deal); type = 'proposal'; break;
+        // case 'dealInvoice': obj = _invoice(deal); type = 'invoice'; break;
     };
-    
-    db.getSiblingDB('tradebrics_prod')[type].insert(clean(obj));
-
+    updatedDeals[type].push(_clean(obj));
 });
 
+// todo: ========================SAVING========================
+_addToDB('rfq');
+// _addToDB(rfp);
+// _addToDB(rfi);
+// _addToDB(po);
+// _addToDB(information);
+// _addToDB(quote);
+// _addToDB(proposal);
+// _addToDB(invoice);
+
+// todo: ========================SCHEMAS========================
 function _rfq(deal) {
     return {
-        _id = deal._id,
-        code = _isPopulated(deal.dealRfq, 'rfqNumber'),
-        image = _isPopulated(_isPopulated(deal.dealRfq, 'rfqImage'), 'content'),
-        recipients =_cleanArray(_isPopulated(deal, 'dealRecipients')),
-        items = _items(_isPopulated(deal.dealRfq, 'rfqItems')),
-        note = _isPopulated(deal.dealRfq, 'rfqNote'),
-        industry = _isPopulated(deal.dealRfq, 'rfqIndustry'),
-        document = _isPopulated(_isPopulated(deal.dealRfq, 'rfqDocumentation'), 'content'),
-        address =_clean({
+        _id: deal._id,
+        code: _isPopulated(deal.dealRfq, 'rfqNumber'),
+        image: _isPopulated(_isPopulated(deal.dealRfq, 'rfqImage'), 'content'),
+        recipients: _cleanArray(_isPopulated(deal, 'dealRecipients')),
+        items: _items(_isPopulated(deal.dealRfq, 'rfqItems')),
+        note: _isPopulated(deal.dealRfq, 'rfqNote'),
+        industry: _isPopulated(deal.dealRfq, 'rfqIndustry'),
+        document: _isPopulated(_isPopulated(deal.dealRfq, 'rfqDocumentation'), 'content'),
+        address: _clean({
             short: _isPopulated(_isPopulated(deal.dealRfq, 'rfqAddress'), 'short'),
             street: _isPopulated(_isPopulated(deal.dealRfq, 'rfqAddress'), 'street'),
             city: _isPopulated(_isPopulated(deal.dealRfq, 'rfqAddress'), 'short'),
@@ -34,7 +55,7 @@ function _rfq(deal) {
             country: _isPopulated(deal.dealRfq, 'rfqCountry'),
             code: _isPopulated(_isPopulated(deal.dealRfq, 'rfqAddress'), 'postal'),
         }),
-        meeting =_clean({
+        meeting: _clean({
             time: _isPopulated(_isPopulated(deal.dealRfq, 'rfqBriefingMeeting'), 'meetingTime'),
             date: _isPopulated(_isPopulated(deal.dealRfq, 'rfqBriefingMeeting'), 'meetingDate'),
             short: _isPopulated(_isPopulated(deal.dealRfq, 'rfqBriefingMeeting'), 'short'),
@@ -42,21 +63,21 @@ function _rfq(deal) {
             city: _isPopulated(_isPopulated(deal.dealRfq, 'rfqBriefingMeeting'), 'administrativeTwo'),
             province: _isPopulated(_isPopulated(deal.dealRfq, 'rfqBriefingMeeting'), 'administrativeOne'),
         }),
-        issueDate = _isPopulated(deal.dealRfq, 'rfqIssueDate'),
-        expiryDate = _isPopulated(deal.dealRfq, 'rfqDeadlineDate'),
-        updatedDate = _isPopulated(deal, 'dealLastUpdated'),
-        publishDate = _isPopulated(deal, 'dealPublishDate'),
-        lead = _isPopulated(deal, 'dealLead'),
-        public = _isPopulated(deal, 'dealPublic'),
-        stage = _isPopulated(deal, 'dealStage'),
-        business =_clean({
+        issueDate: _isPopulated(deal.dealRfq, 'rfqIssueDate'),
+        expiryDate: _isPopulated(deal.dealRfq, 'rfqDeadlineDate'),
+        updatedDate: _isPopulated(deal, 'dealLastUpdated'),
+        publishDate: _isPopulated(deal, 'dealPublishDate'),
+        lead: _isPopulated(deal, 'dealLead'),
+        public: _isPopulated(deal, 'dealPublic'),
+        stage: _isPopulated(deal, 'dealStage'),
+        business: _clean({
             _id: _isPopulated(_isPopulated(deal, 'dealBusiness'), '_id'),
             image: _isPopulated(_isPopulated(_isPopulated(deal, 'dealBusiness'), 'businessImage'), 'content'),
-                legalName: _isPopulated(_isPopulated(deal, 'dealBusiness'), 'businessLegalName'),
-                regNumber: _isPopulated(_isPopulated(deal, 'dealBusiness'), 'businessRegistrationNumber'),
-                vatNumber: _isPopulated(_isPopulated(deal, 'dealBusiness'), 'businessVatNumber'),
+            legalName: _isPopulated(_isPopulated(deal, 'dealBusiness'), 'businessLegalName'),
+            regNumber: _isPopulated(_isPopulated(deal, 'dealBusiness'), 'businessRegistrationNumber'),
+            vatNumber: _isPopulated(_isPopulated(deal, 'dealBusiness'), 'businessVatNumber'),
         }),
-        user =_clean({
+        user: _clean({
             _id: _isPopulated(_isPopulated(deal, 'dealUser'), '_id'),
             display: _isPopulated(_isPopulated(deal, 'dealUser'), 'userFirstName') + ' ' + _isPopulated(_isPopulated(deal, 'dealUser'), 'userLastName'),
             email: _isPopulated(_isPopulated(deal, 'dealUser'), 'userEmail'),
@@ -64,11 +85,41 @@ function _rfq(deal) {
     };
 }
 
+function _rfi(deal) {
+    // return {}
+}
+
+function _po(deal) {
+    // return {}
+}
+
+function _information(deal) {
+    // return {}
+}
+
+function _quote(deal) {
+    // return {}
+}
+
+function _proposal(deal) {
+    // return {}
+}
+
+function _invoice(deal) {
+    // return {}
+}
+
+// todo: ========================TOOLS========================
+function _addToDB(col) {
+    if (updatedDeals[col].length > 0)
+        db.getSiblingDB('tradebrics_prod')[col].insertMany(updatedDeals[col]);
+}
+
 function _items(items) {
     let result = [];
     if (items.length > 0) {
         items.forEach(item => {
-            result.push(clean({
+            result.push(_clean({
                 ref: _isPopulated(item, 'itemRef'),
                 name: _isPopulated(item, 'itemName'),
                 quantity: _isPopulated(item, 'itemQuantity'),
@@ -89,7 +140,7 @@ function _taxItems(items) {
     let result = [];
     if (items.length > 0) {
         items.forEach(item => {
-            result.push(clean({
+            result.push(_clean({
                 tax: _isPopulated(item, 'taxAmount'),
                 rate: _isPopulated(item, 'taxRate'),
             }));
@@ -104,7 +155,7 @@ function _infoItems(items) {
     let result = [];
     if (items.length > 0) {
         items.forEach(item => {
-            result.push(clean({
+            result.push(_clean({
                 desc: _isPopulated(item, 'itemDescription'),
                 file: _isPopulated(_isPopulated(item, 'itemFile'), 'content'),
                 name: _isPopulated(item, 'itemName'),
